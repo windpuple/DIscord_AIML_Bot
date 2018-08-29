5294ade86d63eb18a267d3132c2305ab347f5051
windpuple <windpuple@naver.com>
2018-08-29 10:17:03 +0900

    learing is fixed for escape each user concurrently saying

--- WInd_chatbot.py
+++ WInd_chatbot.py
@@ -14,7 +14,11 @@ BRAIN_FILE="./AIML_BOT/brain.dump"
 
 k = aiml.Kernel()
 
+
 Learn_flag = 0
+Sentence_Num = 0
+target_Num = 0
+user_name = "initial"
 
 file = codecs.open('./AIML_BOT/Sentence_Num_Upkeep.cfg', 'r','utf-8')
 Sentence_Num = int(file.read())
@@ -72,8 +76,8 @@ async def on_message(message):
     global Learn_flag
     global Sentence_Num
     global target_Num
+    global user_name
     
-
     if message.author.bot: #만약 메시지를 보낸사람이 봇일 경우에는
         return None #동작하지 않고 무시합니다.
 
@@ -84,9 +88,9 @@ async def on_message(message):
         Client_sentence = str(message.content)
         Client_sentence_Count = len(Client_sentence)-1
 
-        second_third_aspect = ["난","넌"," 난 "," 넌 ","내"," 내 ","색깔","네 "," 네 "]
+        second_third_aspect = ["난","넌"," 난 "," 넌 ","내"," 내 ","색깔","네 "," 네 ","<",">"]
         for i in second_third_aspect:
-            print(i)
+            #print(i)
             
             if i == "난" and Client_sentence[:len("&난")] == "&난":
                 Client_sentence = Client_sentence.replace(i,'&나는')
@@ -115,15 +119,21 @@ async def on_message(message):
             if Client_sentence.find(i) and i == "색깔":
                 Client_sentence = Client_sentence.replace(i,'색')
 
-            print(Client_sentence)
+            if Client_sentence.find(i) and i == "<":
+                Client_sentence = Client_sentence.replace(i,'left_arrow')
+
+            if Client_sentence.find(i) and i == ">":
+                Client_sentence = Client_sentence.replace(i,'right_arrow')
+
+            #print(Client_sentence)
 
 
 
         josa = ["은 ","는 ","이 ","가 ","을 ","를 ","야 ","의 ","이야?"]
         for i in josa:
-            print(i)
+            #print(i)
             Client_sentence = Client_sentence.replace(i,' '+i)
-            print(Client_sentence)
+            #print(Client_sentence)
 
 #        finish_word = ["야"]
 #        for i in finish_word:
@@ -134,36 +144,46 @@ async def on_message(message):
 #
 #미래에 종사로 끝나느 경우가 추가되면 사용.
 
-        finish_word = ["야","야?","?","니?","니"]
+        finish_word = ["야","야?","?","니?","니","지","지?"]
         for i in finish_word:
             if i == "야?" and Client_sentence[len(Client_sentence)-len("야?"):] == "야?":
-                print(i)
+                #print(i)
                 Client_sentence = Client_sentence.replace(i,' 야')
-                print(Client_sentence)
+                #print(Client_sentence)
 
             if i == "야" and Client_sentence[len(Client_sentence)-len("야"):] == "야":
-                print(i)
+                #print(i)
                 Client_sentence = Client_sentence.replace(i,' 야')
-                print(Client_sentence)
+                #print(Client_sentence)
 
             if i == "?" and Client_sentence[len(Client_sentence)-len("?"):] == "?":
-                print(i)
+                #print(i)
                 Client_sentence = Client_sentence.replace(i,'')
-                print(Client_sentence)
+                #print(Client_sentence)
 
             if i == "니?" and Client_sentence[len(Client_sentence)-len("니?"):] == "니?":
-                print(i)
+                #print(i)
                 Client_sentence = Client_sentence.replace(i,' 니')
-                print(Client_sentence)
+                #print(Client_sentence)
 
             if i == "니" and Client_sentence[len(Client_sentence)-len("니"):] == "니":
-                print(i)
+                #print(i)
                 Client_sentence = Client_sentence.replace(i,' 니')
-                print(Client_sentence)
- 
+                #print(Client_sentence)
+            
+            if i == "지?" and Client_sentence[len(Client_sentence)-len("지?"):] == "지?":
+                #print(i)
+                Client_sentence = Client_sentence.replace(i,' 지')
+                #print(Client_sentence)
+
+            if i == "지" and Client_sentence[len(Client_sentence)-len("지"):] == "지":
+                #print(i)
+                Client_sentence = Client_sentence.replace(i,' 지')
+                #print(Client_sentence)
+
 
         k.setPredicate("user_name","<@"+id+">", id) # 말하는 사람의 ID를 읽어서 봇에게 저장
-
+        k.setPredicate("id_name",id, id)
         # 봇의 아이덴티티 설정 
         k.setBotPredicate("bot_name","Wind")
         k.setBotPredicate("bot_sex","남자")
@@ -190,24 +210,46 @@ async def on_message(message):
         k.setBotPredicate("bot_likekpop","댄싱인더나이트")
         k.setBotPredicate("bot_likekpopsinger","트와이스,블랙핑크")
 
-        
-
-        
-
-        print(Client_sentence[0])
+  
+        #print(Client_sentence[0])
 
         if Client_sentence[0] == "&":
              Client_sentence = Client_sentence.replace("&","")
              Client_sentence = Client_sentence.lstrip()
             
-             print(Client_sentence)
-
-      
+             #print(Client_sentence)
 
         if(Learn_flag == 0):
+            Client_sentence = Client_sentence.upper() 
+
+        adjust_word = [" 나 이 "," 몇살 "]
+        for i in adjust_word:
+            if i == " 나 이 " and Client_sentence.find(" 나 이 "):
+                #print(i)
+                Client_sentence = Client_sentence.replace(i,' 나이 ')
+                #print(Client_sentence)
+
+            if i == " 몇살 " and Client_sentence.find(" 몇살 "):
+                #print(i)
+                Client_sentence = Client_sentence.replace(i,' 몇 살 ')
+                #print(Client_sentence)
+
+        print("Learn_flag1 ",Learn_flag)
+        print("Sentence_Num1 ",Sentence_Num)
+        print("target_Num1 ",target_Num)
+        print("user_name1 ",user_name)
+
+        print(Client_sentence)
+
+        if(Learn_flag == 0 and k.getPredicate("user_name",id) != user_name):
             if k.respond(Client_sentence, id):
-                await client.send_message(channel, k.respond(Client_sentence, id)) #봇은 해당 채널에 '커맨드' 라고 말합니다.
+
+                bot_answer = k.respond(Client_sentence, id).replace('left_arrow','<')
+                bot_answer = bot_answer.replace('right_arrow','>')
+
+                await client.send_message(channel, bot_answer) #봇은 해당 채널에 '커맨드' 라고 말합니다.
             else:
+
                 file = codecs.open('./AIML_BOT/standard/MisUnderstand_Sentence.txt', 'a','utf-8')    # hello.txt 파일을 쓰기 모드(w)로 열기. 파일 객체 반환
                 file.write(str(Sentence_Num)+" "+k.getPredicate("user_name",id)+" "+Client_sentence+"\n")      # 파일에 문자열 저장
                 file.close()                     # 파일 객체 닫기
@@ -217,19 +259,85 @@ async def on_message(message):
                 target_Num = Sentence_Num
                 Sentence_Num = Sentence_Num+1
                 Learn_flag = 1
+                user_name = user_name.replace(user_name,k.getPredicate("user_name",id))   
+
+                print("Learn_flag2 ",Learn_flag)
+                print("Sentence_Num2 ",Sentence_Num)
+                print("target_Num2 ",target_Num)
+                print("user_name2 ",user_name)
+
+                file = codecs.open('./AIML_BOT/Sentence_Num_Upkeep.cfg', 'w','utf-8')
+                file.write(str(Sentence_Num))
+                file.close()
+        
+        elif(Learn_flag == 0 and k.getPredicate("user_name",id) == user_name):
+            if k.respond(Client_sentence, id):
                 
+                bot_answer = k.respond(Client_sentence, id).replace('left_arrow','<')
+                bot_answer = bot_answer.replace('right_arrow','>')
+
+                await client.send_message(channel, bot_answer) #봇은 해당 채널에 '커맨드' 라고 말합니다.
+            
+            else:
+
+                file = codecs.open('./AIML_BOT/standard/MisUnderstand_Sentence.txt', 'a','utf-8')    # hello.txt 파일을 쓰기 모드(w)로 열기. 파일 객체 반환
+                file.write(str(Sentence_Num)+" "+k.getPredicate("user_name",id)+" "+Client_sentence+"\n")      # 파일에 문자열 저장
+                file.close()                     # 파일 객체 닫기
+                await client.send_message(channel, "아직은 이해 할수 없는 말입니다. 하지만 데이터베이스에 기록하여 지능의 성장에 쓰도록 하겠습니다.")
+                await client.send_message(channel, "그러면 제가 대답을 어떻게 해야 하는지 알려주세요.")
+
+                target_Num = Sentence_Num
+                Sentence_Num = Sentence_Num+1
+                Learn_flag = 1
+                user_name = user_name.replace(user_name,k.getPredicate("user_name",id))   
+
+                print("Learn_flag2 ",Learn_flag)
+                print("Sentence_Num2 ",Sentence_Num)
+                print("target_Num2 ",target_Num)
+                print("user_name2 ",user_name)
+
                 file = codecs.open('./AIML_BOT/Sentence_Num_Upkeep.cfg', 'w','utf-8')
                 file.write(str(Sentence_Num))
                 file.close()
 
-        elif(Learn_flag == 1):
+        elif(Learn_flag == 1 and k.getPredicate("user_name",id) != user_name):
+            if k.respond(Client_sentence, id):
+                
+                bot_answer = k.respond(Client_sentence, id).replace('left_arrow','<')
+                bot_answer = bot_answer.replace('right_arrow','>')
+
+                await client.send_message(channel, bot_answer) #봇은 해당 채널에 '커맨드' 라고 말합니다.               
+            
+            else:
+
+                file = codecs.open('./AIML_BOT/standard/MisUnderstand_Sentence.txt', 'a','utf-8')    # hello.txt 파일을 쓰기 모드(w)로 열기. 파일 객체 반환
+                file.write(str(Sentence_Num)+" "+k.getPredicate("user_name",id)+" "+Client_sentence+"\n")      # 파일에 문자열 저장
+                file.close()                     # 파일 객체 닫기
+                await client.send_message(channel, "아직은 이해 할수 없는 말입니다. 하지만 데이터베이스에 기록하여 지능의 성장에 쓰도록 하겠습니다.")
+                await client.send_message(channel, "그러면 제가 대답을 어떻게 해야 하는지 알려주세요.")
+
+                target_Num = Sentence_Num
+                Sentence_Num = Sentence_Num+1
+                Learn_flag = 1
+                user_name = user_name.replace(user_name,k.getPredicate("user_name",id))   
+
+                print("Learn_flag2 ",Learn_flag)
+                print("Sentence_Num2 ",Sentence_Num)
+                print("target_Num2 ",target_Num)
+                print("user_name2 ",user_name)
+
+                file = codecs.open('./AIML_BOT/Sentence_Num_Upkeep.cfg', 'w','utf-8')
+                file.write(str(Sentence_Num))
+                file.close()
+
+        elif(Learn_flag == 1 and k.getPredicate("user_name",id) == user_name):
                 file = codecs.open('./AIML_BOT/standard/MisUnderstand_Sentence.txt', 'r','utf-8')    # hello.txt 파일을 쓰기 모드(w)로 열기. 파일 객체 반환
                 loop = 0
                 for MisUnderstand_Sentence_line in file:
 
                     #MisUnderstand_Sentence_line = row.readline()
                 
-                    print(MisUnderstand_Sentence_line)
+                    #print(MisUnderstand_Sentence_line)
 
                     
                     readline_split = MisUnderstand_Sentence_line.split()
@@ -237,17 +345,17 @@ async def on_message(message):
 
                     target_Num_Count = len(str(target_Num))+23
                         
-                    print("loop ",loop)
-                    print("Sentence_Num ",str(target_Num))
-                    print("readline_split0 ",readline_split[0])
+                    #print("loop ",loop)
+                    #print("Sentence_Num ",str(target_Num))
+                    #print("readline_split0 ",readline_split[0])
                         
                     if readline_split[0] == str(target_Num):
                             file = codecs.open(
                                 './AIML_BOT/standard/MisUnderstand_Answer_Sentence.aiml', 'r','utf-8')
                             Answer_Sentence_line = file.read()
-                            print("aiml erase1 ",Answer_Sentence_line)
+                            #print("aiml erase1 ",Answer_Sentence_line)
                             Answer_Sentence_line = Answer_Sentence_line.replace('</aiml>', '')
-                            print("aiml erase2 ",Answer_Sentence_line)
+                            #print("aiml erase2 ",Answer_Sentence_line)
                             file.close()
 
                             file = codecs.open(
@@ -281,10 +389,6 @@ async def on_message(message):
                 Learn_flag = 0
                 
             
-
-
-
-
         
     elif message.content.startswith('!종료'):
             await client.send_message(message.channel, '종료합니다.')
@@ -296,9 +400,9 @@ async def my_background_task():
         channel = discord.Object(id='481774355085524994')
         
         while not client.is_closed:
-            timemeassage = "Wind BOT이 활동한지 {}시간이 지났습니다.".format(time)
+            timemeassage = "Wind BOT이 활동한지 {}일이 지났습니다.".format(time)
             await client.send_message(channel, timemeassage)
-            await asyncio.sleep(60*60) 
+            await asyncio.sleep(60*60*24) 
             time += 1
             
 try:
